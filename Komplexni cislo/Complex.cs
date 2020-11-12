44e61363065d7a5864e1f10faabb8359ff372748
nightguarder <73370044+nightguarder@users.noreply.github.com>
2020-11-12 12:16:16 +0100

    Complex Calculator Version 1.2.
    Using switch Method

--- Komplexni cislo/Complex.cs
+++ Komplexni cislo/Complex.cs
@@ -17,40 +17,32 @@ namespace Komplexni_cislo
             double d = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Zadana cisla jsou: " +"("+a +" "+c+")"+" " +"("+b+ "i " +d+"i"+")");
             Console.WriteLine("Vyberte operaci:\n" +"scitani= +\n" +"odecitani= -\n" +"nasobeni= *\n" +"deleni= /\n");
-            string uzivatel = Console.ReadLine();       //Convert to Int? 
+            string uzivatel =Console.ReadLine();       //Convert to Int? 
             
-            if (uzivatel == "+")
+            switch (uzivatel)
             {
-                System.Numerics.Complex number = new System.Numerics.Complex(a, b);
-                System.Numerics.Complex number2 = new System.Numerics.Complex(c, d);
-                Console.WriteLine(System.Numerics.Complex.Add(number, number2));
-                Console.WriteLine("{0}" + " "+"{1}",number,number2);
-                return;
-            }
-            else if (uzivatel == "-")
-            {
-                System.Numerics.Complex number1 = new System.Numerics.Complex(a, b);
-                System.Numerics.Complex number2 = new System.Numerics.Complex(c, d);
-                Console.WriteLine(System.Numerics.Complex.Subtract(number1, number2));
-                return;
-            }
-            else if (uzivatel == "*")
-            {
-
-                System.Numerics.Complex number = new System.Numerics.Complex(a, b);
-                System.Numerics.Complex number2 = new System.Numerics.Complex(c, d);
-
-                Console.WriteLine(System.Numerics.Complex.Multiply(number, number2));
-                return;
-            }
-            else if (uzivatel == "/")
-            {
-                System.Numerics.Complex number = new System.Numerics.Complex(a, b);
-                System.Numerics.Complex number2 = new System.Numerics.Complex(c, d);
-
-                Console.WriteLine(System.Numerics.Complex.Divide(number, number2));
-                Console.WriteLine("{0}" + " " + "{1}", number, number2);
-                return;
+                case "+":
+                    System.Numerics.Complex number = new System.Numerics.Complex(a, b);
+                    System.Numerics.Complex number2 = new System.Numerics.Complex(c, d);
+                    Console.WriteLine(System.Numerics.Complex.Add(number, number2));
+                    Console.WriteLine("{0}" + " " + "{1}", number, number2);
+                    return;
+                case "-":
+                    System.Numerics.Complex number1 = new System.Numerics.Complex(a, b);
+                    System.Numerics.Complex number3 = new System.Numerics.Complex(c, d);
+                    Console.WriteLine(System.Numerics.Complex.Subtract(number1, number3));
+                    return;
+                case "*":
+                    System.Numerics.Complex number4 = new System.Numerics.Complex(a, b);
+                    System.Numerics.Complex number5 = new System.Numerics.Complex(c, d);
+                    Console.WriteLine(System.Numerics.Complex.Multiply(number4,number5));
+                    Console.WriteLine("{0}" + " " + "{1}", number4,number5);
+                    return;
+                case "/":
+                    System.Numerics.Complex number6 = new System.Numerics.Complex(a, b);
+                    System.Numerics.Complex number7 = new System.Numerics.Complex(c, d);
+                    Console.WriteLine(System.Numerics.Complex.Divide(number6,number7));
+                    return;
             }
             Console.WriteLine("Děkuji za použití Komplexní kalkulačky.Hezky +den.");
         }
